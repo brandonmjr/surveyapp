299710e5bd4babf12e36c8223f4892ae64ea4e04
mrmaj <brandonmjr@yahoo.com>
2017-12-23 19:48:36 -0500

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require('express');
+var express = require('express');
+var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-const app = express();
+// set the view engine to ejs
+//app.set('view engine', 'ejs');
 
-app.get('/', (req, res) =>{
-	res.send('hey boo')
+// make express look in the public directory for assets (css/js/img)
+//app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.send('wass uuuuppp');
 });
 
-app.listen(5000);
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
